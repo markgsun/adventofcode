7c23fc2c89ef28f7b556b32ba50a21cd19f77be8
Mark Sun <mark.g.sun@gmail.com>
2023-12-04 00:50:21 -0500

    Refactored 4b

--- src/4b-cards.py
+++ src/4b-cards.py
@@ -14,17 +14,15 @@ reps = [1]*len(input_list)
 
 for card, input_str in enumerate(input_list):
     print(input_str)
+    winning = [int(x) for x in input_str.split('|')[0].split(':')[1].split(' ') if x]
+    win_dict = dict.fromkeys(winning, 0)
+    number_list = [int(x) for x in input_str.split('|')[1].split(' ') if x]
     win_tot = 0
-    for _ in range(reps[card]):
-        winning = [int(x) for x in input_str.split('|')[0].split(':')[1].split(' ') if x]
-        win_dict = dict.fromkeys(winning, 0)
-        number_list = [int(x) for x in input_str.split('|')[1].split(' ') if x]
-        win_tot = 0
-        for number in number_list:
-            if number in win_dict:
-                win_tot += 1
-        if card < len(input_list)-1:
-            reps[card+1:card+win_tot+1] = [x + 1 for x in reps[card+1:card+win_tot+1]]
-        total += 1
+    for number in number_list:
+        if number in win_dict:
+            win_tot += 1
+    if card < len(input_list)-1:
+        reps[card+1:card+win_tot+1] = [x + reps[card] for x in reps[card+1:card+win_tot+1]]
+    total += reps[card]
 
 print(total)
