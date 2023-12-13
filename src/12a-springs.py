40fb6bd7fb6f644952d39488357d953373340309
Mark Sun <mark.g.sun@gmail.com>
2023-12-13 09:23:36 -0500

    Completed day 12 exercises

---
+++ src/12a-springs.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+"""12a-springs.py: Advent of Code Day 12: Hot springs"""
+
+__author__ = 'Mark Sun'
+
+# Import utilities
+import utilities as u
+
+# Import input
+input_list = u.import_data('12')
+
+records = [list(x[0]) for x in [x.split() for x in input_list]]
+groups = [[int(y) for y in x[1].split(',')] for x in [x.split() for x in input_list]]
+lines = [list(x) for x in zip(records, groups)]
+
+
+def get_groups(springs_in):
+    blocks_out = []
+    pos = 0
+    while pos < len(springs_in):
+        block_len = 1
+        if springs_in[pos] in ['#']:
+            pos2 = pos + 1
+            while pos2 < len(springs_in):
+                if springs_in[pos2] == springs_in[pos]:
+                    block_len += 1
+                    pos2 += 1
+                else:
+                    break
+            blocks_out += [block_len]
+        pos += block_len
+    return blocks_out
+
+
+def get_permutations(index_in, line_in):
+    springs_in = line_in[0]
+    groups_in = line_in[1]
+    ct = 0
+    springs_a = springs_in.copy()
+    springs_b = springs_in.copy()
+
+    springs_a[index_in] = '#'
+    springs_b[index_in] = '.'
+
+    if '?' not in springs_a:
+        ct += int(get_groups(springs_a) == groups_in)
+        ct += int(get_groups(springs_b) == groups_in)
+        # print('ct: {}, a: {}, b: {}'.format(ct, springs_a, springs_b))
+    else:
+        index_new = springs_a.index('?')
+        ct += get_permutations(index_new, [springs_a, groups_in])
+        ct += get_permutations(index_new, [springs_b, groups_in])
+    return ct
+
+
+total = 0
+for line in lines:
+    total += get_permutations(line[0].index('?'), line)
+
+print('total: {}'.format(total))
---
+++ src/12b-springs.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+"""12b-springs.py: Advent of Code Day 12: Hot springs"""
+
+__author__ = 'Mark Sun'
+
+# Import utilities
+import utilities as u
+
+# Import input
+input_list = u.import_data('12')
+
+springs_raw = [list(x[0]) for x in [x.split() for x in input_list]]
+groups_raw = [[int(y) for y in x[1].split(',')] for x in [x.split() for x in input_list]]
+
+lines = [list(x) for x in zip(springs_raw, groups_raw)]
+
+
+def get_permutations(springs, blocks, i, ib, ln):
+    key = (i, ib, ln)
+    if key in memo.keys():
+        return memo[key]
+
+    # End conditions
+    if i == len(springs):
+        if ib == len(blocks) and ln == 0:
+            return 1
+        elif ib == len(blocks)-1 and blocks[ib] == ln:
+            return 1
+        else:
+            return 0
+    # Recursion
+    ct = 0
+    a = 0
+    b = 0
+    if springs[i] == '.':
+        if ln == 0:
+            ct = get_permutations(springs, blocks, i+1, ib, ln)
+        elif ln > 0 and ib < len(blocks) and ln == blocks[ib]:
+            ct = get_permutations(springs, blocks, i+1, ib+1, 0)
+    elif springs[i] == '#':
+        ct = get_permutations(springs, blocks, i+1, ib, ln+1)
+    elif springs[i] == '?':
+        # Change to #
+        a = get_permutations(springs, blocks, i+1, ib, ln+1)
+        # Change to .
+        if ln == 0:
+            b = get_permutations(springs, blocks, i+1, ib, ln)
+        elif ln > 0 and ib < len(blocks) and ln == blocks[ib]:
+            b = get_permutations(springs, blocks, i+1, ib+1, 0)
+
+        ct = a+b
+
+    memo[key] = ct
+    return ct
+
+
+total = 0
+for line in lines:
+    memo = {}
+    springs_in = ((line[0]+['?'])*5)[:-1]
+    blocks_in = line[1]*5
+    total += get_permutations(springs_in, blocks_in, 0, 0, 0)
+
+print('total: {}'.format(total))
